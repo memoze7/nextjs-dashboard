5a90b05230b4c213fea7813e0a0c61c2bcc79f00
Guillermo Pérez <guillermojperezp@gmail.com>
2023-11-02 10:42:09 -0400

    add suspense for streaming data

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import {fetchCardData} from "@/app/lib/data";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,6 +15,9 @@ const iconMap = {
 };
 
 export default async function Cards() {
+
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+
   return (
     <>
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,16 +2,12 @@ import {ArrowPathIcon} from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import {lusitana} from '@/app/ui/fonts';
+import {fetchLatestInvoices} from "@/app/lib/data";
 
-export default async function LatestInvoices({latestInvoices}: {
-  latestInvoices: {
-    amount: string,
-    id: string,
-    name: string,
-    image_url: string,
-    email: string
-  }[]
-}) {
+export default async function LatestInvoices() {
+
+  const latestInvoices = await fetchLatestInvoices();
+
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
